2b9a1eb30796233916672d4551de2a0f14b5441d
Rendy Febry <rendyfebry@gmail.com>
2017-10-22 01:20:31 +0700

    Wiring Login Function

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+	cognito: {
+		USER_POOL_ID: "ap-southeast-1_qfX1sae8Y",
+		APP_CLIENT_ID: "45j6ns2qpanhjvor2qac9240t0"
+	}
+}
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {
+	CognitoUserPool,
+	AuthenticationDetails,
+	CognitoUser
+} from "amazon-cognito-identity-js"
+
+import config from "../config"
 import "./Login.css";
 
 export default class Login extends Component {
@@ -22,8 +29,32 @@ export default class Login extends Component {
 		});
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
+
+		try {
+			await this.login(this.state.email, this.state.password);
+			alert("Logged in");
+		} catch (e) {
+			alert(e);
+		}
+	}
+
+	login(email, password) {
+		const userPool = new CognitoUserPool({
+			UserPoolId: config.cognito.USER_POOL_ID,
+			ClientId: config.cognito.APP_CLIENT_ID
+		});
+		const user = new CognitoUser({ Username: email, Pool: userPool });
+		const authenticationData = { Username: email, Password: password };
+		const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+		return new Promise((resolve, reject) =>
+			user.authenticateUser(authenticationDetails, {
+				onSuccess: result => resolve(),
+				onFailure: err => reject(err)
+			})
+		);
 	}
 
 	render() {
